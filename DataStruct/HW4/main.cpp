8e13925b3e360136f52b750ef36226dbd47cf219
chrisliu430 <hearnechris430@gmail.com>
2020-05-09 15:08:34 +0800

    Update

--- DataStruct/HW4/main.cpp
+++ DataStruct/HW4/main.cpp
@@ -1,64 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-vector<vector<bool>> graph(0);
-vector<bool> status(0);
-vector<int> router(0);
-vector<int> number(0);
-vector<int> stack(0);
-vector<int> frequency(0);
-
-int check() {
-    bool breakJudge = false;
-    for (int i = 0; i < status.size(); i++) {
-        if (status[i] == 1 && router[i] >= 1) {
-            breakJudge = true;
-        } else {
-            breakJudge = false;
-            break;
-        }
-    }
-    return breakJudge;
-}
-
-int solve(int n) {
-    int min = INT_MAX, reg, runSize;
-    bool inStatus, writeMin = true, pushJudge;
-    // ---
+void InputProcess(int n) {
+    bool inStatus;
+    vector<bool> node(0);
+    vector<vector<bool>> graph(0);
     while (graph.size() != n) {
-        reg = 0;
-        while (status.size() != n) {
+        while (node.size() != n) {
             cin >> inStatus;
-            status.push_back(inStatus);
+            node.push_back(inStatus);
         }
-        for (int i = 0; i < n; i++) {
-            if (status[i] == 1) {
-                reg++;
-            }
-        }
-        frequency.push_back(reg);
-        graph.push_back(status);
-        status.clear();
+        graph.push_back(node);
+        node.clear();
     }
-    while (status.size() != n) {
+    while (node.size() != n) {
         cin >> inStatus;
-        status.push_back(inStatus);
-        router.push_back(0);
+        node.push_back(inStatus);
     }
-    for (int i = 0; i < n; i++) {
-    }
-    graph.clear();
-    status.clear();
-    router.clear();
-    frequency.clear();
-    cout << min << endl;
-    return 0;
+    // ---
 }
- 
+
 int main(void) {
     int n;
-    while (cin >> n && n > 0) { solve(n); }
+    while (cin >> n && n > 0) InputProcess(n);
     return 0;
-} 
+}
